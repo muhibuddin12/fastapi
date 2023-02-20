de91bff5c972c9ce06837fc288fdfdcd5464885b
Muhibuddin <30959389+muhibuddin12@users.noreply.github.com>
2023-02-20 03:15:38 +0000

    update

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, Path, Body
 from pydantic import BaseModel
 import uvicorn
 
@@ -11,9 +11,9 @@ class Item(BaseModel):
     description: str
     category: str
 
-@app.post("/items/")
-async def create_item(item: Item):
-    return item
+# @app.post("/items/")
+# async def create_item(item: Item):
+#     return item
 
 # using query 
 @app.get("/products")
@@ -30,5 +30,36 @@ async def get_mahasiswa(name: list[str] | None = Query(default=...)):
         results["mahasiswa"].append({"name": value})
     return results
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+
+# @app.get("/items/{item_id}")
+# async def get_item(
+#         item_id: int = Path(title="The ID of the item to get"),
+#         q: str | None = Query(default=None, alias="item-query")
+#     ):
+#     results = {"item_id": item_id}
+#     if q:
+#         results.update({"q": q})
+#     return results
+
+
+@app.put("/items/{item_id}")
+async def update_item(
+    *,
+    item_id: int = Path(title="The ID of the item to get", ge=0, le=1000),
+    q: str | None = None,
+    item: Item | None = None,
+):
+    results = {"item_id": item_id}
+    if q:
+        results.update({"q": q})
+    if item:
+        results.update({"item": item})
+    return results
+
+# @app.put("/items/{item_id}")
+# async def update_item(item_id: int, item: Item = Body(embed=True)):
+#     results = {"item_id": item_id, "item": item}
+#     return results
+
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="0.0.0.0", port=8000)
